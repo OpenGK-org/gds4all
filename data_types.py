09b7b560f9b9ee372e045eb800d389f084b90e0c
Piotr Zdolski <dante383@protonmail.com>
2025-12-02 18:48:14 +0100

    initial commit

---
+++ data_types.py
@@ -0,0 +1,192 @@
+from dataclasses import dataclass, field
+from enum import Enum
+from xml.etree.ElementTree import Element
+
+class ConversionType(Enum):
+	SIMPLE = 1 # just multiply and add
+	SIMPLE_NO_ADD = 2 # only difference between 1 seems to be the lack of add value?
+	BITFIELD_ENUM = 3 # mask and shift, then match with string values from LUT
+
+class Protocol(Enum):
+	J1850_VPW = 0x01
+	J1850_PWM = 0x02
+	ISO_9141  = 0x03
+	ISO_14230 = 0x04
+	CAN       = 0x05
+	ISO_15765 = 0x06
+	SCI_A_TRANS = 0x08
+	SCI_B_ENGINE = 0x09
+	SCI_B_TRANS = 0x0A
+	ISO_9141_2 = 0x10001
+	BOSCH_BCM = 0x100015
+
+@dataclass
+class GdsDataClass:
+	@classmethod
+	def from_xml (self, element: Element):
+		return self._from_xml(element)
+
+@dataclass
+class Conversion(GdsDataClass):
+	type: ConversionType
+	factor: float = 1
+	add: float = 0
+
+	def _equation_factor (self) -> str:
+		if self.factor != 1:
+			return '{:g}*'.format(self.factor)
+		else:
+			return ''
+
+	def _equation_add (self) -> str:
+		if self.add != 0:
+			if self.add < 0:
+				return '+{:g}'.format(abs(self.add))
+			else:
+				return '-{:g}'.format(self.add)
+		else:
+			return ''
+
+	# tunerpro format
+	def to_equation (self) -> str:
+		return '{}X{}'.format(self._equation_factor(), self._equation_add())
+
+	def __repr__ (self) -> str:
+		return self.to_equation()
+
+@dataclass
+class ConversionSimple(Conversion):
+	type: ConversionType = ConversionType.SIMPLE
+
+	def __repr__ (self) -> str: # apparently __repr__ isnt inherited
+		return self.to_equation()
+
+@dataclass
+class ConversionSimpleNoAdd(Conversion): 
+	type: ConversionType = ConversionType.SIMPLE_NO_ADD
+
+	def __repr__ (self) -> str:
+		return self.to_equation()
+
+
+@dataclass
+class ConversionBitfieldEnum(Conversion):
+	type: ConversionType = ConversionType.BITFIELD_ENUM
+	shift: int = 0
+	bitmask: int = 0x0
+	values: list[str] = None
+
+	def __repr__ (self) -> str:
+		return '((X&{}) >> {}) -> [{}]'.format(
+			hex(self.bitmask), 
+			self.shift,
+			'|'.join(self.values)
+		)
+
+@dataclass
+class CurrentDataNode(GdsDataClass):
+	# header: str ??
+	index: int
+	name: str
+
+	request_payload: str # tbh this should probably be bytes
+	response_prefix: str 
+	response_start_position: int # this should be added to self.position if operating on raw response frames
+
+	position: int
+	
+	size: int
+	data_type: str # ??
+
+	min_value: int
+	max_value: int
+
+	unit: str
+	decimal_points: int
+
+	# compid: str ??
+	# freeze: ??
+	# dtc: str ??
+
+	conversion: Conversion
+
+	def __repr__ (self) -> str:
+		return 'CurrentDataNode(i={}, name=\'{}\', position={}, size={}, unit=\'{}\', conversion={})'.format(self.index, self.name, self.position, self.size, self.unit, self.conversion)
+
+@dataclass
+class RequestNode(GdsDataClass):
+	request_payload: str
+	response_prefix: str # XX - wildcard. @todo: convert this to hex and generate a mask. consider different length (it is a prefix after all)
+	index: int | None = field(default=None, repr=False)
+
+	@classmethod
+	def _from_xml (self, node: Element):
+		return self(
+			request_payload=node.get('request'), 
+			response_prefix=node.get('response'),
+			index=node.get('index')
+		)
+
+@dataclass
+class ActuationTestNode(GdsDataClass):
+	# header: ??
+	index: int
+	name: str
+
+	start_condition: str
+	stop_condition: str
+	request_condition: str
+	time: int # seconds?
+	request_time: int # ??? timeout?
+
+	start_request: RequestNode
+	stop_request: RequestNode
+	end_request: RequestNode
+
+	# compid: str ??
+
+	def __repr__ (self) -> str:
+		return('ActuationTestNode(i={}, name=\'{}\', start_condition=\'{}\', start_payload={})'.format(self.index, self.name, self.start_condition, self.start_request.request_payload))
+
+@dataclass
+class Dtc(GdsDataClass):
+	header: str
+	index: str
+	mask: str # should be hex?
+	freeze_index: str | None
+
+	# commcode: str ?? 
+	# compid: str ??
+
+	@classmethod
+	def _from_xml (self, node: Element):
+		return self(
+			index=node.get('index'),
+			header=node.get('header'),
+			mask=node.get('mask'),
+			freeze_index=node.findall('freezeindex')[0].get('index') # @todo?
+		)
+
+@dataclass
+class DtcFunction(GdsDataClass):
+	requests: list[RequestNode]
+	dtcs: list[Dtc]
+
+@dataclass
+class SupportedFunction(GdsDataClass):
+	index: int
+	description: str
+
+@dataclass
+class CommunicationSetup(GdsDataClass):
+	tx_id: int | None
+	rx_id: int # / module id
+	vss_channel: str # ???
+	protocol: Protocol
+
+	supported_functions: list[SupportedFunction]
+
+	# @TODO
+
+	def __repr__ (self) -> str:
+		return 'CommunicationSetup(tx_id={}, rx_id={}, protocol={})'.format(hex(self.tx_id) if self.tx_id else 'None', hex(self.rx_id) if self.rx_id else 'None', self.protocol.name)
---
+++ gds4all.py
@@ -0,0 +1,430 @@
+import xml.etree.ElementTree as ET
+import sys, glob, os, textwrap
+from data_types import Conversion, ConversionSimple, ConversionBitfieldEnum, ConversionType, CurrentDataNode, RequestNode, ActuationTestNode, Dtc, DtcFunction, SupportedFunction, Protocol, CommunicationSetup
+
+# used for debugging. set sys.argv to, say, 26, 3, 1. 
+# a 2006 2.0 tiburon will be selected: vehicle 26, year 3, variant 1
+prefilled_interactive_selects = []
+
+TERMINAL_WIDTH = os.get_terminal_size().columns
+
+messages = {}
+collections = {}
+
+def _get (xml, param: str, default = None, cast_to = None, cast_args: tuple = None) -> str | None:
+	value = xml.get(param, default)
+	
+	if value == '':
+		return default
+
+	if cast_to:
+		try:
+			value = cast_to(value, *cast_args)
+		except (TypeError, ValueError) as e:
+			value = None
+	
+	return value
+
+def _print (*args, indent: int = 0, **kwargs):
+	return print(' '*indent, *args, **kwargs)
+	
+def interactive_select (table, prompt):
+	if (len(prefilled_interactive_selects) > 0):
+		index = int(prefilled_interactive_selects.pop(0))
+		print('Prefilled: {}'.format(index))
+		return table[index]
+
+	try:
+		return table[int(input(prompt))]
+	except (KeyError, IndexError, ValueError):
+		print('Invalid choice! Try again')
+		return interactive_select(table, prompt)
+
+def load_vehicles ():
+	tree = ET.parse('decrypted_xef/vehiclesdata_HME.xml')
+	root = tree.getroot()
+	vehicles = []
+
+	for child in root:
+		vehicles.append(child)
+
+	return vehicles
+
+def load_messages (dataset: str):
+	tree = ET.parse(dataset).getroot()
+	for keyvalue in tree:
+		if keyvalue.attrib['attr'] not in messages:
+			messages[keyvalue.attrib['attr']] = {}
+		messages[keyvalue.attrib['attr']][keyvalue.attrib['key']] = keyvalue.attrib['desc']
+
+def load_collections (dataset: str):
+	tree = ET.parse(dataset).getroot()
+
+	for collection in tree:
+		collections[collection.tag] = {}
+		for keyvalue in collection:
+			collections[collection.tag][keyvalue.attrib['key']] = keyvalue.attrib['desc']
+
+def load_ecu (ecu_code: str):
+	try:
+		tree = ET.parse('decrypted_xef/{}.xml'.format(ecu_code))
+		if len(tree.getroot()) == 0:
+			print('File was found but apparently contains no root')
+			raise FileNotFoundError # this is weird, happens with EHST.xml: <?xml version="1.0" encoding="UTF-8"?><systemtree systemid="EHST" mmcid="" version="1.0" date="2005-04-18" author="GIT" history=""/>
+	except FileNotFoundError:
+		if (ecu_code[-1] != '0'):
+			try:
+				return load_ecu(glob.glob('decrypted_xef/{}*.xml'.format(ecu_code))[0].replace('decrypted_xef/', '').replace('.xml', '')) # lmao
+			except IndexError:
+				return False
+		else:
+			return False
+
+	root = tree.getroot()
+	if len(root) == 0:
+		return None
+
+	return root
+
+def get_message (key: int, attribute: str = None):
+	try:
+		return messages[attribute][key]
+	except KeyError:
+		return ''
+
+def get_from_collection (key: str, collection_name: str = 'currentdata'):
+	try:
+		return collections[collection_name][key]
+	except KeyError:
+		return ''
+
+def get_lookup_table (identifier: int) -> list|None:
+	lut_filenames = [f'Lut/{identifier}.lut', f'Lut/{identifier}.LUT'] # consistency [cool]
+	values = []
+
+	for lut_filename in lut_filenames:
+		try:
+			with open(lut_filename, 'r') as f:
+				for value in f.readlines():
+					values.append(value.lstrip().rstrip())
+			return values
+		except FileNotFoundError:
+			pass
+	return None
+
+
+def handle_step (bus, step):
+	print('Step #{}'.format(step.attrib['stepno']))
+	print(get_message(step.attrib['stepdesc'], attribute='stepdesc'))
+
+	message = step.findall('message')[0]
+	if (message.attrib['messageindex'] != '0'):
+		print(get_message(message.attrib['messageindex'], attribute='messageindex'))
+
+	keystrings = step.findall('keystring')
+
+	print('\n')
+	if (keystrings[0].attrib['type'] != '0'):
+		for key, keystring in enumerate(keystrings):
+			print('[{}] {}'.format(key, keystring.attrib['filename']))
+		keystring = interactive_select(keystrings, '=========\nSelect: ')
+
+		if 'jumpstep' in keystring.attrib:
+			return int(keystring.attrib['jumpstep'])
+
+	for request in message.findall('request'):
+		for response in request:
+			print('    Expected response format: {}\n'.format(response.attrib['responsevalue']))
+			print('    We\'re looking for some data with size {}, starting at offset {}'.format(response.attrib['datasize'], response.attrib['startposition']))
+
+			for comcode in response.findall('comcode'):
+				operation_name = 'equals to' if comcode.attrib['compare'] == '1' else 'does not equal' 
+				print('    - If this data {} {}, then we jump to step {}'.format(operation_name, comcode.attrib['code'], comcode.attrib['jumpstep']))
+
+	return False
+
+def main ():
+	print('loading messages..')
+	load_messages('decrypted_xef/add-ENG.xml')
+	load_messages('decrypted_xef/keyvalue.xml')
+	load_messages('decrypted_xef/keyvalueUnit.xml') 
+
+	print('loading collections..')
+	load_collections('decrypted_xef/dtc-ENG.xml')
+
+	if not os.path.isdir('Lut'):
+		print('[!] Lookup table files not found, enum values won\'t be extracted.')
+		print('You can find them in the Lut directory in main GDS dir')
+
+	print('loading vehicles..')
+
+	vehicles = load_vehicles()
+
+	for key, vehicle in enumerate(vehicles):
+		print('[{}] - {}'.format(key, vehicle.get('modeldesc')))
+
+	vehicle = interactive_select(vehicles, 'Select vehicle: ')
+	print('selected vehicle: {}'.format(vehicle.get('modeldesc')))
+
+	# manufacturer, lang, vehicle type, model vin
+	vehicle_years = vehicle[0][0][0][0][0]
+	
+	for key, vehicle_year in enumerate(vehicle_years):
+		print('[{}] - {}'.format(key, vehicle_year.get('modelyr')))
+
+	vehicle_variants = interactive_select(vehicle_years, 'Select year: ')
+
+	for key, variant in enumerate(vehicle_variants):
+		print('[{}] - {}'.format(key, variant.get('enginedesc')))
+
+	vehicle_systems = interactive_select(vehicle_variants, 'Select engine type: ')
+
+	for key, system in enumerate(vehicle_systems):
+		print('\n[{}] - {}'.format(key, system.get('sysitemdesc')))
+		print('    {}'.format('/'.join([subsystem.get('syssubitemdesc') for subsystem in system])))
+
+	vehicle_system = interactive_select(vehicle_systems, 'Select system: ')
+	
+	if (len(vehicle_system) > 1):
+		for key, subsystem in enumerate(vehicle_system):
+			print('[{}] {}'.format(key, subsystem.get('syssubitemdesc')))
+		vehicle_system = interactive_select(vehicle_system, 'Select subsystem: ')
+	else:
+		vehicle_system = vehicle_system[0]
+
+	print('\n\n\nSelected system: {}'.format(vehicle_system.get('syssubitemdesc')))
+	if len(vehicle_system) > 1:
+		print('There is more than one ECU assigned to this subsystem: {}'.format(', '.join([ecu.get('ecucode') for ecu in vehicle_system])))
+
+	for ecu_node in vehicle_system:
+		ecu_code = ecu_node.attrib['ecucode']
+		ecu = load_ecu(ecu_code)
+
+		if ecu == False:
+			print('Failed to load ECU {}. Trying to match a wildcard now: {}'.format(ecu_code, ecu_code[:-1]))
+			ecu = load_ecu(ecu_code[:-1])
+
+		if ecu == None:
+			print('[!] Failed to load ECU definition: {}'.format(ecu_code))
+			continue
+
+		print('\nLoaded ECU: {}, definition last modified: {}'.format(ecu.get('systemid'), ecu.get('date')))
+
+		print('=== Communication ===')
+
+		commset = ecu.findall('commset')[0]
+		print('    Protocol used: {}'.format(get_message(commset.get('protocolid'), 'protocolid')))
+		try:
+			protocol = Protocol(int(_get(commset, 'protocolid'), 16))
+		except ValueError as e:
+			protocol = None
+			print(e)
+			continue
+
+
+		# what the fuck is wrong with them?
+		tester_id = _get(commset, 'testerid', cast_to=int, cast_args=(16,))#.lstrip().split(' ')[0]
+		module_id = _get(commset, 'moduleid', cast_to=int, cast_args=(16,))#.lstrip().split(' ')[0]
+
+		#tester_id = int(tester_id, 16)#None if len(tester_id) == 0 else int(tester_id, 16)
+		#module_id = int(module_id, 16)#None if len(module_id) == 0 else int(module_id, 16)
+
+
+		communication_setup = CommunicationSetup(
+			tx_id=tester_id,
+			rx_id=module_id,
+			vss_channel=_get(commset, 'vsschannel'),
+			protocol=protocol,
+			supported_functions=[
+				SupportedFunction(
+					index=_get(x, 'index'), 
+					description=get_message(_get(x, 'index'), attribute='index')
+				) for x in commset.findall('funcsupport')[0]
+			]
+		)
+		_print('{}'.format(communication_setup), indent=4)
+
+		for requestnode in commset.findall('startcomm/requestnode'):
+			if (requestnode.attrib['request'] != ''):
+				_print('Connect payload: {}'.format(requestnode.attrib['request']), indent=4)
+				_print('Expected response: {}'.format(requestnode.attrib['response']), indent=4)
+
+		print('\n=== This ECU supports following functions: ===')
+		for function_declaration in communication_setup.supported_functions:
+			_print('- {} ({})'.format(function_declaration.description, function_declaration.index), indent=4)
+
+		_print('=== Current data function ===', indent=4)
+		current_data_nodes = {}
+		for node in ecu.findall('currentdata/currentdatanode'):
+			convrule = node.findall('convrule')[0]
+			try:
+				convtype = ConversionType(int(_get(convrule, 'convtype')))
+			except ValueError as e:
+				print(e)
+				continue
+
+			if convtype == ConversionType.SIMPLE:
+				add = float(_get(convrule, 'B', '0'))
+				if add != 0:
+					add = add * -1 # invert the value as GDS defs use subtraction
+				conversion = ConversionSimple(
+					factor=float(_get(convrule, 'A')), 
+					add=add
+				)
+			elif convtype == ConversionType.SIMPLE_NO_ADD:
+				conversion = ConversionSimple(
+					factor=float(_get(convrule, 'A')),
+				)
+			elif convtype == ConversionType.BITFIELD_ENUM:
+				values = get_lookup_table(_get(convrule, 'A'))
+				if not values:
+					_print('Failed to load LUT', indent=8)
+					values = []
+
+				conversion = ConversionBitfieldEnum(
+					add=float(_get(convrule, 'B', '0')),
+					shift=int(_get(convrule, 'C')),
+					bitmask=int(_get(convrule, 'D'), 16),
+					values=values
+				)
+
+			data_node = CurrentDataNode(
+				index=_get(node, 'index'),
+				name=get_from_collection(_get(node, 'index'), 'currentdata'),
+
+				request_payload=_get(node, 'requestcode'),
+				response_prefix=_get(node, 'response'),
+				response_start_position=_get(node, 'startpos'),
+
+				position=_get(node, 'realpos', int),
+				size=int(_get(node, 'datasize')),
+				data_type=_get(node, 'datatype'),
+				min_value=int(_get(node, 'minvalue'), 16),
+				max_value=int(_get(node, 'maxvalue'), 16),
+				unit=get_message(_get(node, 'unit'), attribute='unit'),
+				decimal_points=_get(node, 'floatrange', int),
+				conversion=conversion
+			)
+			if data_node.request_payload != '8211F12101A6':
+				continue
+
+			current_data_nodes[int(data_node.position)] = data_node
+
+
+		for data_node in [v for k, v in sorted(current_data_nodes.items())]:
+			_print('- {}: ({})'.format(data_node.name, data_node.index), indent=8)
+			_print('Payload: {}'.format(data_node.request_payload), indent=12)
+			_print('Response format: {}'.format(data_node.response_prefix), indent=12)
+			
+
+			
+			if (_get(node, 'unit', 0, int) != 0):
+				_print('Unit: {} ({})'.format(data_node.unit, node.get('unit')), indent=12)
+			_print('Position: {} Size: {}'.format(data_node.position, data_node.size), indent=12)
+			_print('Precision: {}'.format(data_node.decimal_points), indent=12)
+			_print('Conversion: {} ({})'.format(data_node.conversion, convtype), indent=12)
+
+			print()
+
+		_print('=== Actuation test function ===', indent=4)
+		for node in ecu.findall('actuationtest/actuationtestnode'):
+			try:
+				start_request = RequestNode.from_xml(node.findall('starttest')[0][0])
+			except IndexError:
+				start_request = None
+
+			try:
+				stop_request = RequestNode.from_xml(node.findall('stoptest')[0][0])
+			except IndexError:
+				stop_request = None
+
+			try:
+				end_request = RequestNode.from_xml(node.findall('endtest')[0][0])
+			except IndexError:
+				end_request = None
+
+			test_node = ActuationTestNode(
+				index=_get(node, 'index', int),
+				name=get_from_collection(_get(node, 'index'), 'actuationtest'),
+
+				start_condition=get_from_collection(_get(node, 'actuationtestcondition'), 'actuationtest'),
+				stop_condition=get_from_collection(_get(node, 'stopcondition'), 'actuationtest'),
+				request_condition=get_from_collection(_get(node, 'requestcondition'), 'actuationtest'),
+
+				time=_get(node, 'actuationtesttime', int),
+				request_time=_get(node, 'requesttime', int),
+
+				start_request=start_request,
+				stop_request=stop_request,
+				end_request=end_request,
+
+			)
+			_print(str(test_node), indent=8)
+			_print('Start payload: {}'.format(test_node.start_request.request_payload), indent=12)
+
+		print('\n    === DTCs: ===')
+		for node in ecu.findall('dtc'):
+			dtc_func = DtcFunction(
+				requests=[
+					RequestNode.from_xml(x) for x in node.findall('requestcodetree')[0].findall('requestnode')
+				],
+				dtcs=[
+					Dtc.from_xml(x) for x in node.findall('dtcitemtree')
+				]
+			)
+
+			_print('Request: {}'.format(dtc_func.requests[0]), indent=8)
+			for dtc in dtc_func.dtcs:
+				_print('{}'.format(str(dtc)), indent=8)
+
+
+		print('\n    === "addfunction" functions: ===')
+		addfunction_functions = ecu.findall('addfunction')
+		for key, node in enumerate(addfunction_functions):
+			_print('[{}] {}'.format(key, get_message(node.get('fuctionindex'), attribute='index')), indent=8)
+			_print('{}'.format(get_message(node.get('fuctiondesc'), attribute='fuctiondesc')), indent=12)
+			_print('Steps:', indent=12)
+			for step in sorted(node.findall('step'), key=lambda s: int(_get(s, 'stepno'))):
+				_print('#{} {}'.format(_get(step, 'stepno'), get_message(_get(step, 'stepdesc'), attribute='stepdesc')), indent=16)
+				message = step.findall('message')[0]
+				if (message.get('messageindex') != '0'):
+					message_text = get_message(message.get('messageindex'), attribute='messageindex')
+					message_lines = message_text.split('\\n')
+					for i, line in enumerate(message_lines):
+						try:
+							if line == '' and message_lines[i+1] == '':
+								continue
+						except IndexError:
+							pass
+
+						line = line.strip().replace('\n', '')
+
+						# make sure no line is crossing the indent, i hate that
+						for line in textwrap.wrap(line, width=(TERMINAL_WIDTH-20)):
+							_print('{}'.format(line), indent=20)
+					
+
+		_stephandlingmethods = '''continue # ACHTUNG!
+		selected_function = interactive_select(addfunction_functions, 'Select function: ')
+
+		print('\n\n\n==========')
+		print(get_message(selected_function.attrib['fuctiondesc'], attribute='fuctiondesc'))
+
+		function_steps = sorted(selected_function.findall('step'), key=lambda s: int(s.attrib['stepno']))
+		next_step_no = handle_step(bus, function_steps[0])
+		while next_step_no:
+			try:
+				next_step = function_steps[next_step_no-1]
+				next_step_no = handle_step(bus, next_step)
+				if (next_step_no == 0):
+					break
+			except (IndexError, KeyError):
+				break'''
+
+	if (len(vehicle_system) > 1):
+		print('[!] Be advised: more than 1 ECU definition was processed and displayed')
+
+if __name__ == '__main__':
+	if len(sys.argv) > 1:
+		prefilled_interactive_selects = sys.argv[1:]
+	main()
